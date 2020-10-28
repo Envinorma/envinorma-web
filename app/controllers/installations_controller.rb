487cd2aca1cb5b59ede8994be9b768299b3212ed
Lisa Durand <lisa@superdudu.net>
2020-10-28 16:07:35 +0100

    add show, edit, update for installation

--- app/controllers/installations_controller.rb
+++ app/controllers/installations_controller.rb
@@ -1,5 +1,24 @@
 class InstallationsController < ApplicationController
+  before_action :set_installation, except: :index
+
   def index
     @installations = Installation.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @installation.update(date: params[:installation][:date])
+    redirect_to installation_path(@installation)
+  end
+
+  private
+
+  def set_installation
+    @installation = Installation.find(params[:id])
   end
 end
