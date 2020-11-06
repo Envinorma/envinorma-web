4849b51c97ff346ae29c585559df595f7af645a1
Lisa Durand <lisa@superdudu.net>
2020-11-06 14:29:04 +0100

    create form and retrieve params with prescriptions

--- app/controllers/arretes_controller.rb
+++ app/controllers/arretes_controller.rb
@@ -5,9 +5,22 @@ class ArretesController < ApplicationController
     @arretes = @installation.arretes
   end
 
+  def generate_doc_with_prescriptions
+    prescriptions = prescriptions_params.to_h
+    prescriptions_text = []
+    prescriptions.each do |id, text|
+      prescriptions_text << text
+    end
+    #to do generate .odt with prescriptions
+  end
+
   private
 
   def set_installation
     @installation = Installation.find(params[:id])
+  end
+
+  def prescriptions_params
+    params["prescriptions"].permit!
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'installations#index'
   resources :installations
   get '/installations/:id/arretes', to: 'arretes#index', as: 'arretes'
+  post '/installations/:id/arretes', to: 'arretes#generate_doc_with_prescriptions', as: 'generate_doc'
 end
