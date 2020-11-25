7bb36bd41ba05242acbd589659bbc1643c828e19
Lisa Durand <lisa@superdudu.net>
2020-11-25 15:24:51 +0100

    add sommaire

--- app/javascript/components/active_nav.js
+++ app/javascript/components/active_nav.js
@@ -1,9 +1,13 @@
 window.addEventListener('DOMContentLoaded', () => {
   let mainNavLinks = document.querySelectorAll(".am_nav a");
   let mainSections = document.querySelectorAll(".container section");
+  let mainNavSommaire = document.querySelectorAll(".sommaire .display-sommaire");
 
   let lastId;
   let cur = [];
+
+  mainNavLinks[0].classList.add("current");
+  mainNavSommaire[0].classList.add("current");
   // This should probably be throttled.
   // Especially because it triggers during smooth scrolling.
   // https://lodash.com/docs/4.17.10#throttle
@@ -17,15 +21,19 @@ window.addEventListener('DOMContentLoaded', () => {
     let fromTop = window.scrollY;
 
     mainNavLinks.forEach(link => {
-      let section = document.querySelector(link.hash);
+      // let section = document.querySelector(link.hash);
+      let section = document.querySelector(`section${link.hash}`);
+      let sommaire = document.querySelector(`div${link.hash}-sommaire`);
 
       if (
         section.offsetTop <= fromTop &&
         section.offsetTop + section.offsetHeight > fromTop
       ) {
         link.classList.add("current");
+        sommaire.classList.add("current");
       } else {
         link.classList.remove("current");
+        sommaire.classList.remove("current");
       }
     });
   });
