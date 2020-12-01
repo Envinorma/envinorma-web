b7d84d4c091b10e901d2897fb943351582d45739
Lisa Durand <lisa@superdudu.net>
2020-12-01 12:39:34 +0100

    create classement store table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_101721) do
+ActiveRecord::Schema.define(version: 2020_12_01_113728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 2020_12_01_101721) do
     t.string "aida_url"
     t.string "legifrance_url"
     t.jsonb "summary"
+  end
+
+  create_table "classement_stores", force: :cascade do |t|
+    t.string "rubrique"
+    t.string "regime"
+    t.string "alinea"
+    t.string "activite"
+    t.string "seuil"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "classements", force: :cascade do |t|
