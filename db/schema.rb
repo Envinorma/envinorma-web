db82219b6a6482d7632a66cc03373f5f08141342
Lisa Durand <lisa@superdudu.net>
2020-12-01 12:44:41 +0100

    create installation_store table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_113728) do
+ActiveRecord::Schema.define(version: 2020_12_01_114327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,13 @@ ActiveRecord::Schema.define(version: 2020_12_01_113728) do
     t.string "seuil"
     t.index ["arrete_id"], name: "index_classements_on_arrete_id"
     t.index ["installation_id"], name: "index_classements_on_installation_id"
+  end
+
+  create_table "installation_stores", force: :cascade do |t|
+    t.string "name"
+    t.jsonb "data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "installations", force: :cascade do |t|
