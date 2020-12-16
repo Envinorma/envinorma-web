50fbaa437bd7c53f15e8edf0d5b3d312ef0fcb47
Lisa Durand <lisa@superdudu.net>
2020-12-16 16:30:44 +0100

    seed arrete from json

--- app/models/arrete.rb
+++ app/models/arrete.rb
@@ -2,8 +2,6 @@ class Arrete < ApplicationRecord
   has_many :classements
   has_many :installations, through: :classements
 
-  validates :name, presence: true
-
   def data
     JSON.parse(super.to_json, object_class: OpenStruct)
   end
