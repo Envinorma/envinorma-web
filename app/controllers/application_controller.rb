5f27aae7dccc80f2b650407bd372076efc2bbaf2
Lisa Durand <lisa@superdudu.net>
2021-01-06 18:14:08 +0100

    small fix if user session dont match with a user in BDD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   private
 
   def set_user_if_needed
-    @user = User.find(session[:user_id]) if session[:user_id]
+    if session[:user_id] && User.exists?(session[:user_id])
+      @user = User.find(session[:user_id])
+    end
   end
 end
