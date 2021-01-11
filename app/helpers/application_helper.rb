f7eac4a3a6d675280ba7578b92dca25809e36d3d
Lisa Durand <lisa@superdudu.net>
2021-01-11 16:10:13 +0100

    change the way to retrieve classement to display alinea reference if present in arrete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,12 @@ module ApplicationHelper
     if arrete.is_a? EnrichedArrete
       arrete = Arrete.find(arrete.arrete_id)
     end
-    classement = Classement.find (installation.classements.pluck(:id) & arrete.classements.pluck(:id)).first
-    " - #{classement.rubrique} #{classement.regime}"
+    classement_ref = Classement.find (installation.classements.pluck(:id) & arrete.classements.pluck(:id)).first
+    classement_from_arrete_data = arrete.data.classements.select {|classement| classement.rubrique.include? classement_ref.rubrique}.first
+    if classement_from_arrete_data.alinea.present?
+      " - #{classement_from_arrete_data.rubrique} #{classement_from_arrete_data.regime} al. #{classement_from_arrete_data.alinea}"
+    else
+      " - #{classement_from_arrete_data.rubrique} #{classement_from_arrete_data.regime}"
+    end
   end
 end
