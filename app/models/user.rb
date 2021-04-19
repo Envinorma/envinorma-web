b19523295f1dfec4aaf06c649dffd8c0b4d463d9
Lisa Durand <lisa@superdudu.net>
2021-04-19 11:03:44 +0200

    check checkbox alinea  if user has saved the prescription in BDD

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@
 class User < ApplicationRecord
   has_many :installations, dependent: :destroy
   has_many :prescriptions, dependent: :destroy
+
+  def prescription_checked?(alinea_id)
+    prescriptions.map(&:alinea_id).include?(alinea_id)
+  end
 end
