856f2f8e2c1600d2a670769e2ac7d15d0c651708
remidbs <remi.delbouys@laposte.net>
2021-04-27 11:36:38 +0200

    fixed and improved odf_helper tests

--- spec/helpers/odf_helper_spec.rb
+++ spec/helpers/odf_helper_spec.rb
@@ -55,38 +55,64 @@ RSpec.describe '#compute_cell_content' do
 end
 
 RSpec.describe '#compute_cell_content' do
-  it 'returns cell content when called with one prescription' do
+  it 'returns concatenated prescription content when called with several prescriptions' do
     prescription1 = Prescription.new(
       reference: 'ref',
       content: 'line 3',
       alinea_id: '0',
-      from_am_id: nil,
+      from_am_id: 'am-id',
       text_reference: 'AP 2020',
-      rank: '1',
+      rank: '1.1.2',
       user_id: 0
     )
     prescription2 = Prescription.new(
       reference: 'ref',
       content: "line 1\nline 2",
       alinea_id: '0',
-      from_am_id: nil,
+      from_am_id: 'am-id',
       text_reference: 'AP 2020',
       rank: '0',
       user_id: 0
     )
     prescriptions = [prescription1, prescription2]
-    result = 'line 1<text:line-break/>line 2<text:line-break/><text:line-break/>line 3'
+    result = 'line 3<text:line-break/><text:line-break/>line 1<text:line-break/>line 2'
     expect(compute_cell_content(prescriptions)).to eq result
   end
 end
 
 RSpec.describe '#merge_prescriptions_with_same_ref' do
-  it 'returns cell content when called with one prescription' do
+  it 'returns grouped and sorted prescription contents when called with several prescriptions' do
+    prescription1 = Prescription.new(
+      reference: 'ref',
+      content: 'line 3',
+      alinea_id: '0',
+      from_am_id: 'am-id',
+      text_reference: 'am-id',
+      rank: '1.1.2',
+      user_id: 0
+    )
+    prescription2 = Prescription.new(
+      reference: 'ref',
+      content: "line 1\nline 2",
+      alinea_id: '0',
+      from_am_id: 'am-id',
+      text_reference: 'am-id',
+      rank: '0',
+      user_id: 0
+    )
+    prescriptions = [prescription1, prescription2]
+    result = { 'am-id - ref' => 'line 1<text:line-break/>line 2<text:line-break/><text:line-break/>line 3' }
+    expect(merge_prescriptions_with_same_ref(prescriptions)).to eq result
+  end
+end
+
+RSpec.describe '#merge_prescriptions_with_same_ref' do
+  it 'returns grouped and sorted prescription contents when called with several prescriptions' do
     prescription1 = Prescription.new(
       reference: 'Art. 1',
       content: 'line 3',
       alinea_id: '2',
-      from_am_id: nil,
+      from_am_id: 'am-id',
       text_reference: 'AM 2020',
       rank: '1',
       user_id: 0
@@ -95,7 +121,7 @@ RSpec.describe '#merge_prescriptions_with_same_ref' do
       reference: 'Annexe 2.',
       content: "line 1\nline 2",
       alinea_id: '0',
-      from_am_id: nil,
+      from_am_id: 'am-id',
       text_reference: 'AP 2009',
       rank: '0',
       user_id: 0
