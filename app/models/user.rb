1c552939be1c03909e1f8f1d77541e00ad5dc7a9
Lisa Durand <lisa@superdudu.net>
2021-04-29 16:33:39 +0200

    realign checkbox select_all and rename method to check the checkbox

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :installations, dependent: :destroy
   has_many :prescriptions, dependent: :destroy
 
-  def prescription_exist?(installation, alinea_id)
+  def prescription_checked?(installation, alinea_id)
     prescriptions.where(installation_id: installation.id).map(&:alinea_id).include?(alinea_id)
   end
 
