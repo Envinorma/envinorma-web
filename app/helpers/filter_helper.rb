0f090cfac661a4cc5c455322ea6a9d339bed7117
remidbs <remi.delbouys@laposte.net>
2021-04-30 17:32:01 +0200

    sort prescriptions by increasing creation date

--- app/helpers/filter_helper.rb
+++ app/helpers/filter_helper.rb
@@ -3,7 +3,7 @@
 module FilterHelper
   def self.sort_and_group(prescriptions)
     result = {}
-    prescriptions.sort_by(&:type).group_by(&:text_reference).each do |text_reference, group|
+    prescriptions.sort_by(&:created_at).sort_by(&:type).group_by(&:text_reference).each do |text_reference, group|
       result[text_reference] = group.sort_by(&:rank_array).group_by(&:reference)
     end
     result
