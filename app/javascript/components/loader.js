730bc4310c74297e770258cbdd03fa9d647cb760
Lisa Durand <lisa@superdudu.net>
2021-06-18 14:39:55 +0200

    remove loader on link click and disable button after click on installation show to avoid double click

--- app/javascript/components/loader.js
+++ app/javascript/components/loader.js
@@ -1,12 +1,3 @@
 window.addEventListener('load', () => {
   document.querySelector(".spinner").classList.remove("active");
 });
-
-window.addEventListener('DOMContentLoaded', () => {
-  const links = document.querySelectorAll('.js_loader__link');
-  for (const link of links) {
-    link.addEventListener('click', function() {
-      document.querySelector(".spinner").classList.add("active");
-    });
-  }
-});
