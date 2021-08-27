8a94844c0dc35199e0a0c297266c2124ffd8fe0a
remidbs <remi.delbouys@laposte.net>
2021-08-27 18:50:21 +0200

    fixed APs initializations

--- spec/factories/aps.rb
+++ spec/factories/aps.rb
@@ -6,6 +6,8 @@ FactoryBot.define do
     date { '27/04/2021'.to_date }
     installation_s3ic_id { '0065.06351' }
     georisques_id { 'D/b/888cd0e0031343ebae4c0e28ef5d8bab' }
+    ocr_status { 'SUCCESS' }
+    size { 4.megabytes }
     installation
   end
 end
