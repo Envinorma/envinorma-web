d123ff6a046efaefead64ca881a7da8e24622b72
Lisa Durand <lisa@superdudu.net>
2021-09-15 10:28:06 +0200

    add select_all on installation page and improve select_all

--- app/javascript/components/select_all_checkbox.js
+++ app/javascript/components/select_all_checkbox.js
@@ -1,4 +1,34 @@
-const updateCheckboxes = (event) => {
+const setStateSelectAllCheckbox = (checkbox_select_all) => {
+  const checkboxes = document.querySelectorAll("." + checkbox_select_all.id);
+  var id = checkbox_select_all.id
+
+  const checkboxes_checked = document.querySelectorAll(`.${id}:checked`)
+
+  if (checkboxes.length === checkboxes_checked.length) {
+      checkbox_select_all.checked = true
+  } else {
+      checkbox_select_all.checked = false
+  }
+}
+
+const updateStateSelectAllCheckbox = (event) => {
+  const checkbox = event.target
+  const all_checkbox_class = Array.from(checkbox.classList)
+  const class_name_select_all = all_checkbox_class.filter((class_name) => class_name.startsWith("select_all"));
+  const id_target = class_name_select_all[0]
+  const checkbox_select_all = document.querySelector(`#${id_target}`)
+
+  if (checkbox_select_all == null) return;
+
+  if (checkbox.checked) {
+    setStateSelectAllCheckbox(checkbox_select_all)
+  }
+  else {
+    checkbox_select_all.checked = false
+  }
+}
+
+const updateStateChildCheckboxes = (event) => {
   const checkbox_select_all = event.target;
   const checkboxes = document.querySelectorAll("." + checkbox_select_all.id);
 
@@ -11,7 +41,14 @@ const updateCheckboxes = (event) => {
 };
 
 window.addEventListener("DOMContentLoaded", () => {
+  // all checkbox .select_all
   document.querySelectorAll(".select_all").forEach((checkbox_select_all) => {
-    checkbox_select_all.addEventListener("change", updateCheckboxes);
+    setStateSelectAllCheckbox(checkbox_select_all)
+    checkbox_select_all.addEventListener("change", updateStateChildCheckboxes);
   });
+
+  // all checkbox child of .select_all
+  document.querySelectorAll('input[class*="select_all_"]').forEach((checkbox) => {
+    checkbox.addEventListener("click", updateStateSelectAllCheckbox);
+  })
 });
