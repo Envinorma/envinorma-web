015ce0d2d5bf1c508f6525e6ae0b4cb66120758e
remidbs <remi.delbouys@laposte.net>
2021-09-28 16:56:08 +0200

    made alinea recreate low memory

--- app/resources/alinea_manager.rb
+++ app/resources/alinea_manager.rb
@@ -9,16 +9,19 @@ class AlineaManager
     Rails.logger.info 'Recreating alinea store...'
     AlineaStore.delete_all
     ActiveRecord::Base.connection.reset_pk_sequence!(AlineaStore.table_name)
-    AM.all.each_with_index do |am, index|
+    AM.pluck(:id, :data).each_with_index do |am, index|
+      # We avoid using OpenStruct to avoid memory leak
+      am_id, data = am
       Rails.logger.info "Recreated alinea store for am #{index}/#{AM.count}" if (index % 10).zero?
-      create_from_am(am)
+      GC.start if index % 10 == 9 # Force garbage collection every 10 batches for RAM limitations
+      create_from_am(am_id, data)
     end
     Rails.logger.info '...done.'
   end
 
-  def self.create_from_am(arrete_ministeriel)
-    hashes = arrete_ministeriel.data.sections.map.with_index do |section, index|
-      create_hashes_from_am_section(section, index.to_s, arrete_ministeriel.id, nil)
+  def self.create_from_am(am_id, am_data)
+    hashes = am_data['sections'].map.with_index do |section, index|
+      create_hashes_from_am_section(section, index.to_s, am_id, nil)
     end.flatten
     hashes.each { |hash| AlineaStore.new(hash).validate! }
     hashes = hashes[0..20] if ENV['HEROKU_APP_NAME'] == 'envinorma-staging-1'
@@ -26,18 +29,18 @@ class AlineaManager
   end
 
   def self.create_hashes_from_am_section(section, section_rank, am_id, ascendant_topic)
-    topic = ascendant_topic || section.annotations.topic
+    topic = ascendant_topic || section['annotations']['topic']
     section_hash = {
       am_id: am_id,
-      section_id: section.id,
-      section_name: section.reference.name,
-      section_reference: section.reference.nb,
+      section_id: section['id'],
+      section_name: section['reference']['name'],
+      section_reference: section['reference']['nb'],
       section_rank: section_rank,
       topic: topic || TopicHelper::AUCUN
     }
-    section.outer_alineas.map.with_index do |alinea, index_in_section|
+    section['outer_alineas'].map.with_index do |alinea, index_in_section|
       create_from_am_alinea(alinea, index_in_section, section_hash)
-    end.flatten + section.sections.map.with_index do |subsection, subsection_index|
+    end.flatten + section['sections'].map.with_index do |subsection, subsection_index|
       create_hashes_from_am_section(subsection, "#{section_rank}.#{subsection_index}", am_id, topic)
     end.flatten
   end
@@ -46,26 +49,15 @@ class AlineaManager
     section_hash.merge(
       index_in_section: index_in_section,
       content: extract_prescription_content(alinea),
-      is_table: (alinea.text.blank? && alinea.table.present?) || false
+      is_table: (alinea['text'].blank? && alinea['table'].present?) || false
     )
   end
 
   def self.extract_prescription_content(alinea)
-    return alinea.text if alinea.text.present?
+    return alinea['text'] if alinea.fetch('text', nil).present?
 
-    return open_struct_to_hash(alinea.table).to_json if alinea.table.present?
+    return alinea['table'].to_json if alinea.fetch('table', nil).present?
 
     ''
-  end
-
-  def self.open_struct_to_hash(object, hash = {})
-    object.each_pair do |key, value|
-      hash[key] = case value
-                  when OpenStruct then open_struct_to_hash(value)
-                  when Array then value.map { |v| open_struct_to_hash(v) }
-                  else value
-                  end
-    end
-    hash
   end
 end
